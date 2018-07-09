6575d4ea73ceffd47bd4ff7d0c92d69ec9a59a9b
DESKTOP-07LG49U\DELL <nvthien.uit.k10@gmail.com>
2018-07-09 14:18:53 +0700

    change

---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data
+{
+    public class TeduShopDbContext : DbContext
+    {
+        public TeduShopDbContext() : base("TeduShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Post> Posts { set; get; }
+        public DbSet<PostCategorie> PostCategories { set; get; }
+        public DbSet<PostTag> PostTags { set; get; }
+        public DbSet<Products> Products { set; get; }
+
+        public DbSet<ProductCategorie> ProductCategories { set; get; }
+        public DbSet<ProductTag> ProductTags { set; get; }
+        public DbSet<Slide> Slides { set; get; }
+        public DbSet<SupportOnline> SupportOnlines { set; get; }
+        public DbSet<SystemConfig> SystemConfigs { set; get; }
+
+        public DbSet<Tag> Tags { set; get; }
+
+        public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+
+        }
+    }
+}
